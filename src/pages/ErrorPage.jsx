53ee2c903f1f1ff56cbfcc1e9dda199666738e2e
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-07-17 19:07:31 +0600

    error page  added

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom';
+
 const ErrorPage = () => {
   return (
-    <div>
-      Error
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold text-red-500 mb-4">404</h1>
+        <p className="text-2xl mb-8">Oops! Page not found.</p>
+        <Link to="/" className="bg-[#23BE0A] text-white px-6 py-3 rounded-lg hover:bg-success transition duration-300">
+          Go back to Home
+        </Link>
+      </div>
     </div>
   );
 };
